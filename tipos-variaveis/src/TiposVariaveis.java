59e9f98903180bdb215727be7862cde2a010a89d
bnools <b_bruno@live.com>
2023-08-28 17:32:14 -0300

    Operadores teste

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,20 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Estudar tipos primitivos
+        //Estudar classe String que representa texto 
+
+        String meuNome = "Bruno Oliveira";
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.print(numero);   
     }
 }
