24be8310d660d8104799e3b5014f4bd7bf4d1dab
bnools <b_bruno@live.com>
2023-08-31 12:52:03 -0300

    Operador ternário

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,13 +1,13 @@
 public class Operadores {
     public static void main (String[] args) {
-        double soma = 10.5 + 15.7;
-        int subtracao = 113 - 25;
-        int multiplicacao = 20 * 7;     
-        int divisao = 15/3;
-        int modulo = 18 % 3;
-        double resultado = (10 * 7) + (20/4);
+        int a, b;
 
-        System.out.print(divisao);
+        a = 5;
+        b = 6;
+        
+        /*EXEMPLO DE CONDICIONAL IF ELSE */
+        String resultado = a==b ? "verdadeiro" : "falso";
+        System.out.println(resultado);
     }
     
 }
